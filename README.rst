39a72c229be24706f8d4d6a5d562b1b75cbf1a78
Simon Bächler <sb@feinheit.ch>
2013-09-13 15:35:01 +0200

    make it installable

--- README.rst
+++ README.rst
@@ -2,3 +2,5 @@ feincms-filer-content
 =====================
 
 A FeinCMS contenttype package for django-filer
+
+This is still work in progress.
---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup, find_packages
+
+setup(
+	name = 'feincms-filer-content',
+	packages = find_packages(),
+    include_package_data=True,
+    version=__import__('mediafiler').__version__,
+    description='A FeinCMS contenttype package for django-filer',
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+    author='Simon Baechler',
+    author_email='sb@feinheit.ch',
+    url='https://github.com/sbaechler/feincms-filer-content',
+    license='BSD License',
+    platforms=['OS Independent'],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+    ],
+)
